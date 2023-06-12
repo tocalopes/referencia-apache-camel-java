59abc276fbde7cdfa8b93d34b8f45ef1b129d861
Gabriel <gabriellopes112009@gmail.com>
2023-06-12 15:46:42 -0300

    Importando estrutura inicial do projeto Definicição do context camel e da primeira rota Como exemplo, a rota le todos os arquivos da pasta entrada e move para pasta saida

---
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -0,0 +1,29 @@
+package br.com.caelum.camel;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class RotaPedidos {
+
+	public static void main(String[] args) throws Exception {
+
+		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(new RouteBuilder() {
+			@Override
+			public void configure() throws Exception {
+				//file(tipo de arquivo). :pedido nome arquivo | ?delay = observa a cada 5 segundos
+				from("file:pedidos?delay=5s")
+						.to("file:saida");
+			}
+		});
+
+		context.start();
+
+		Thread.sleep(20000);
+		context.start();
+
+
+	}	
+}
