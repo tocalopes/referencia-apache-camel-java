8fc9234332cf947b7ea37fc19660cd8df40cffcc
Gabriel <gabriellopes112009@gmail.com>
2023-06-13 11:18:14 -0300

    Uso da constante exchange para pegar o valor do arquivo e CamelSplitIndex para obter o index e concatenar com o nome

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,7 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
 import org.apache.camel.RoutesBuilder;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -24,7 +25,8 @@ public class RotaPedidos {
 						.marshal()
 							.xmljson()
 						.log("${body}")
-						.setHeader("CamelFileName", simple("${file:name.noext}.json"))
+						//.setHeader("CamelFileName", simple("${file:name.noext}.json"))
+						.setHeader(Exchange.FILE_NAME, simple("${file:name.noext}-${header.CamelSplitIndex}.json"))
 						.to("file:saida");
 			}
 		});
